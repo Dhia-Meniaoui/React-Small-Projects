f9eb7bbd5a24e6c13863ee31b2d71e05b9200747
dhia <diaminiaoui@gmail.com>
2022-03-01 23:02:28 +0100

    01/03

---
+++ App.js
@@ -0,0 +1,12 @@
+import Main from "./components/Main";
+import Navbar from "./components/Navbar";
+import React from "react";
+
+export default function App(){
+    return (
+        <div>
+            <Navbar></Navbar>
+            <Main></Main>
+        </div>
+    )
+}
